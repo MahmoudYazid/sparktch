31f486c6e5b38ffbd9b7684a8527c0cff8f0199b
Mahmoud Yazid <abuelyazid.software@gmail.com>
2025-09-19 14:42:22 +0200

    solve text-bug
    solve text-bug

--- src/app/components/part12/Part12Cards.js
+++ src/app/components/part12/Part12Cards.js
@@ -4,7 +4,7 @@ const Part12Cards = ({image,title,text}) => {
   <img src={image} alt="example" className="w-full h-auto" />
 
   <div className="absolute bottom-[1rem] left-5 bg-[#F5F7FA] px-3 py-1 rounded w-70 h-[10rem] flex flex-col justify-center items-center">
-    <p className="text-lg font-semibold mb-2 text-center">{title}</p>
+    <p className="text-lg font-semibold mb-2 text-center text-black">{title}</p>
     <a href="#" className="text-green-600 font-semibold hover:underline">
       Read more →
     </a>
